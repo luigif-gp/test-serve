9f8d5cd65ccc2023ad26d86834aaf823475482a1
Luigi Garcia <luigifgp@gmail.com>
2022-06-18 15:24:35 +0100

    update enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
-    allowedHeaders: ['*'],
-    origin: `${process.env.ALLOW_ORIGIN}`,
-    credentials: true,
+    allowedHeaders: "*",
+    origin: "*"
   });
   await app.listen(process.env.PORT || 3000);
 }
