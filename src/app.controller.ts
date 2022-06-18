ccacb8f68916a83b707dafdfdf836903481186dc
Luigi Garcia <luigifgp@gmail.com>
2022-06-18 11:10:24 +0100

    api-key auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common';
 import { Body, Controller, Get, Param } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,14 +8,17 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get(':user')
+  @UseGuards(AuthGuard('api-key'))
   getUserRepos(@Param() params): string {
     return this.appService.getUserRepos(params);
   }
   @Get(':user/:repo')
+  @UseGuards(AuthGuard('api-key'))
   getRepo(@Param() params): string {
     return this.appService.getRepo(params);
   }
   @Get(':user/:repo/:search')
+  @UseGuards(AuthGuard('api-key'))
   getRepoData(@Param() params) {
     return this.appService.getRepoData(params);
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
   imports: [
@@ -10,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.development.env',
       isGlobal: true,
     }),
-    HttpModule],
+    HttpModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 
